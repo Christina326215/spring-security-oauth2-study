1967d725ef6fc4dc6ffc984e50bac2191caea784
Chtistina326215 <christina326215@gmail.com>
2025-04-14 18:18:17 +0800

    spring security OAuth 2.0

---
+++ src/main/java/com/sunnyhsu/springsecurityoauth2study/dto/ExchangeTokenRequest.java
@@ -0,0 +1,14 @@
+package com.sunnyhsu.springsecurityoauth2study.dto;
+
+public class ExchangeTokenRequest {
+
+    private String code;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
---
+++ src/test/java/com/sunnyhsu/springsecurityoauth2study/SpringSecurityOauth2StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunnyhsu.springsecurityoauth2study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
